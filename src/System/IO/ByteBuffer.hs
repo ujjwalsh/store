076b7725cf4a72066dc257af67d17906f3c2c48e
Philipp Kant <philipp@fpcomplete.com>
2016-04-20 10:40:01 +0200

    Adopt ByteBuffer to our needs.
    We don't have an upper bound to the message length, so we'll need to be
    able to grow the buffer.
    
    Also, we'll want to use Conduits, where we get new data via ByteStrings,
    the length of which is not under our control.

--- src/System/IO/ByteBuffer.hs
+++ src/System/IO/ByteBuffer.hs
@@ -1,69 +1,208 @@
-{-# LANGUAGE ExistentialQuantification #-}
-{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-|
+Module: System.IO.ByteBuffer
+Description: Provides an efficient buffering abstraction.
 
--- | Buffering abstraction.
+A 'ByteBuffer' is a simple buffer for bytes.  It supports two
+operations: refilling with the contents of a 'ByteString', and
+consuming a fixed number of bytes.
 
-module System.IO.ByteBuffer where
+It is implemented as a pointer, together with counters that keep track
+of the offset and the number of bytes in the buffer.  Note that the
+counters are simple 'IORef's, so 'ByteBuffer's are not thread-safe!
 
-import Control.Exception
-import Data.Word
-import Foreign.Marshal.Alloc
-import Foreign.Marshal.Utils
-import GHC.IO.Handle
-import GHC.Ptr
-import System.IO
+A 'ByteBuffer' is constructed by 'new' with a given starting length,
+and will grow (by repeatedly multiplying its size by 1.5) whenever it
+is being fed a 'ByteString' that is too large.
+-}
 
--- A filling function which is capable of writing to a buffer n bytes.
-type FillBuffer = Ptr Word8 -> Int -> IO Int
+module System.IO.ByteBuffer
+       ( ByteBuffer
+         -- * Allocation and Deallocation
+       , new, free
+         -- * Query for number of available bytes
+       , isEmpty, availableBytes
+         -- * Feeding new input
+       , copyByteString
+         -- * Consuming bytes from the buffer
+       , consume, unsafeConsume
+       ) where
 
--- A buffer into which bytes can be written.
+import           Control.Monad (when)
+import           Control.Monad.IO.Class
+import           Data.ByteString (ByteString)
+import qualified Data.ByteString.Internal as BS
+import           Data.IORef
+import           Data.Word
+import           Foreign.ForeignPtr
+import qualified Foreign.Marshal.Alloc as Alloc
+import           Foreign.Marshal.Utils hiding (new)
+import           GHC.Ptr
+
+-- | A buffer into which bytes can be written.
+--
+-- Invariants:
+--
+-- * @size >= containedBytes >= consumedBytes >= 0@
+--
+-- * The range from @ptr@ to @ptr `plusPtr` size@ will be allocated
+--
+-- * The range from @ptr@ to @ptr `plusPtr` containedBytes@ will
+--   contain bytes previously copied to the buffer
+--
+-- * The buffer contains @containedBytes - consumedBytes@ bytes of
+--   data that have been copied to it, but not yet read.  They are in
+--   the range from @ptr `plusPtr` consumedBytes@ to @ptr `plusPtr`
+--   containedBytes@.
 data ByteBuffer = ByteBuffer
-    { byteBufferPtr :: !(Ptr Word8)
-    , byteBufferFill :: !FillBuffer
-    , byteBufferSize :: !Int
-    }
+    { ptr :: {-# UNPACK #-} !(IORef (Ptr Word8))
+      -- ^ This points to the beginning of the memory allocated for
+      -- the 'ByteBuffer'.
+    , size :: {-# UNPACK #-} !(IORef Int)
+      -- ^ The amount of memory allocated.
+    , containedBytes :: {-# UNPACK #-} !(IORef Int)
+      -- ^ The number of bytes that the 'ByteBuffer' currently holds.
+    , consumedBytes :: {-# UNPACK #-} !(IORef Int)
+      -- ^ The number of bytes that have already been consumed.
+    } -- deriving Show
 
--- | Allocates a new ByteBuffer with a given buffer size filling from
--- the given FillBuffer.
-newByteBuffer
-    :: Int -- ^ Size of buffer to allocate.
-    -> FillBuffer -- ^ The filling function to use.
-    -> IO ByteBuffer -- ^ The byte buffer.
-newByteBuffer size fillBuffer = do
-    ptr <- mallocBytes size
-    let !byteBuffer = ByteBuffer ptr fillBuffer size
+isEmpty :: MonadIO m => ByteBuffer -> m Bool
+isEmpty bb = liftIO $ (==0) <$> availableBytes bb
+
+-- | Number of available bytes in a 'ByteBuffer' (that is, bytes that
+-- have been copied to, but not yet read from the 'ByteBuffer'.
+availableBytes :: MonadIO m => ByteBuffer -> m Int
+availableBytes ByteBuffer{..} =
+    liftIO $ (-) <$> readIORef containedBytes
+                 <*> readIORef consumedBytes
+
+-- | The number of bytes that can be appended to the buffer, without
+-- resetting it.
+freeCapacity :: ByteBuffer -> IO Int
+freeCapacity ByteBuffer{..} =
+    (-) <$> readIORef size
+        <*> readIORef containedBytes
+
+-- -- | Allocates a new ByteBuffer with a given buffer size filling from
+-- -- the given FillBuffer.
+new :: MonadIO m
+    => Int -- ^ Size of buffer to allocate.
+    -> m ByteBuffer -- ^ The byte buffer.
+new size = liftIO $ do
+    newPtr <- Alloc.mallocBytes size >>= newIORef
+    newSize <- newIORef size
+    contained <- newIORef 0
+    consumed <- newIORef 0
+    let !byteBuffer = ByteBuffer { ptr = newPtr
+                                 , size = newSize
+                                 , containedBytes = contained
+                                 , consumedBytes = consumed
+                                 }
     return byteBuffer
 
 -- | Free a byte buffer.
-freeByteBuffer :: ByteBuffer -> IO ()
-freeByteBuffer = free . byteBufferPtr
+free :: ByteBuffer -> IO ()
+free ByteBuffer{..} = readIORef ptr >>= Alloc.free
 
--- | Fill the byte buffer with more available bytes. You must specify
--- how many bytes you've consumed from the buffer. Unconsumed bytes
--- will be shifted to the beggining of the buffer and more will be
--- filled after it.
-fillByteBuffer
-    :: ByteBuffer -- ^ Byte buffer to fill.
-    -> Int        -- ^ How many bytes consumed by the user of this function.
-    -> IO Int     -- ^ How many new bytes available.
-fillByteBuffer (ByteBuffer ptr fill size) consumed =
-    if consumed == 0 || consumed == size
-       then fill ptr size
-       else do let unconsumed = size - consumed
-               moveBytes ptr (plusPtr ptr consumed) unconsumed
-               available <- fill (plusPtr ptr unconsumed) (size - unconsumed)
-               return (unconsumed + available)
+-- | Reset a 'ByteBuffer', i.e. copy all the bytes that have not yet
+-- been consumed to the front of the buffer.
+reset :: ByteBuffer -> IO ()
+reset ByteBuffer{..} = do
+    contained <- readIORef containedBytes
+    consumed <- readIORef consumedBytes
+    curPtr <- readIORef ptr
+    let available = contained - consumed
+    moveBytes curPtr (curPtr `plusPtr` consumed) available
+    writeIORef containedBytes available
+    writeIORef consumedBytes 0
 
--- | Make a 'FillBuffer' for a 'Handle'.
-handleFillBuffer :: Handle -> FillBuffer
-handleFillBuffer h = \ptr size -> hGetBuf h ptr size
+-- | Make sure the buffer is at least @minSize@ bytes long.
+--
+-- In order to avoid havong to enlarge the buffer too often, we double
+-- its size until it is at least @minSize@ bytes long.
+enlargeByteBuffer :: ByteBuffer
+                 -> Int
+                 -- ^ minSize
+                 -> IO ()
+enlargeByteBuffer ByteBuffer{..} minSize = do
+    curSize <- readIORef size
+    curPtr <- readIORef ptr
+    when (curSize < minSize) $ do
+        let newSize = head . dropWhile (<minSize) $
+                      iterate (ceiling . (*(1.5 :: Double)) . fromIntegral) curSize
+        -- possible optimisation: since reallocation might copy the
+        -- bytes anyway, we could discard the consumed bytes,
+        -- basically 'reset'ting the buffer on the fly.
+        ptr' <- Alloc.reallocBytes curPtr newSize
+        writeIORef ptr ptr'
+        writeIORef size newSize
 
--- | Opens the given file in 'ReadMode', provides a 'FillBuffer' for
--- that file, and afterwards closes the file.
-withFileFillBuffer :: FilePath -> (FillBuffer -> IO a) -> IO a
-withFileFillBuffer fp cont =
-    bracket (openFile fp ReadMode)
-            hClose
-            (cont . handleFillBuffer)
+-- | Copy the contents of a 'ByteString' to a 'ByteBuffer'.
+--
+-- If necessary, the 'ByteBuffer' is enlarged and/or already consumed
+-- bytes are dropped.
+copyByteString :: MonadIO m => ByteBuffer -> ByteString -> m ()
+copyByteString bb@(ByteBuffer{..}) bs@(BS.PS _ _ bsSize) = liftIO $ do
+    -- if the byteBuffer is too small, resize it.
+    curSize <- readIORef size      -- total capacity
+    available <- availableBytes bb -- bytes not yet consumed
+    when (curSize < bsSize + available) (enlargeByteBuffer bb (bsSize + available))
+    -- if it is currently too full, reset it
+    capacity <- freeCapacity bb
+    when (capacity < bsSize) (reset bb)
+    -- now we can safely copy.
+    unsafeCopyByteString bb bs
 
+-- | Copy the contents of a 'ByteString' to a 'ByteBuffer'. No bounds
+-- checks are performed.
+unsafeCopyByteString :: ByteBuffer -> ByteString -> IO ()
+unsafeCopyByteString (ByteBuffer{..}) (BS.PS bsFptr bsOffset bsSize) = do
+    curPtr <- readIORef ptr
+    contained <- readIORef containedBytes
+    withForeignPtr bsFptr $ \ bsPtr ->
+        copyBytes (curPtr `plusPtr` contained)
+                  (bsPtr `plusPtr` bsOffset)
+                  bsSize
+    writeIORef containedBytes (contained + bsSize)
+
+-- | Try to get a pointer to @n@ bytes from the 'ByteBuffer'.
+--
+-- Note that the pointer should be used before any other actions are
+-- performed on the 'ByteBuffer'. It points to some address within the
+-- buffer, so operations such as enlarging the buffer or feeding it
+-- new data will change the data the pointer points to.  This is why
+-- this function is called unsafe.
+unsafeConsume :: MonadIO m
+        => ByteBuffer
+        -> Int
+        -- ^ n
+        -> m (Either Int (Ptr Word8))
+        -- ^ Will be @Left missing@ when there are only @n-missing@
+        -- bytes left in the 'ByteBuffer'.
+unsafeConsume bb@(ByteBuffer{..}) n = liftIO $ do
+    available <- availableBytes bb
+    if available < n
+        then return $ Left (n - available)
+        else do
+             curPtr <- readIORef ptr
+             consumed <- readIORef consumedBytes
+             writeIORef consumedBytes (consumed + n)
+             return $ Right (curPtr `plusPtr` consumed)
+
+-- | As `unsafeConsume`, but instead of returning a `Ptr` into the
+-- contents of the `ByteBuffer`, it returns a `ByteString` containing
+-- the next @n@ bytes in the buffer.  This involves allocating a new
+-- 'ByteString' and copying the @n@ bytes to it.
+consume :: MonadIO m
+        => ByteBuffer
+        -> Int
+        -> m (Either Int ByteString)
+consume bb n = do
+    mPtr <- unsafeConsume bb n
+    case mPtr of
+        Right ptr -> do
+            bs <- liftIO . BS.create n $ \ bsPtr ->
+                copyBytes bsPtr ptr n
+            return (Right bs)
+        Left missing -> return (Left missing)
