40c23ecf50d95dfd40cded84a9fc0e8dba110f5c
Philipp Kant <philipp@fpcomplete.com>
2016-04-28 11:04:39 +0200

    Additional tests for Data.Store.Streaming.
    - Tests that peekMessage behaves correctly when given input in chunks
    - Tests that Exceptions get thrown on incomplete messages.

--- test/Data/Store/StreamingSpec.hs
+++ test/Data/Store/StreamingSpec.hs
@@ -1,25 +1,31 @@
+{-# LANGUAGE LambdaCase #-}
 module Data.Store.StreamingSpec where
 
-import Control.Monad.Trans.Resource
-import Data.Store.Streaming
-import Test.Hspec.SmallCheck
-import Test.SmallCheck
-import Test.Hspec
-import qualified Data.Conduit.List as C
-import Data.Conduit ((=$=), ($$))
+import           Control.Monad.Trans.Resource
 import qualified Data.ByteString as BS
-import Data.List (unfoldr)
-import Data.Monoid
+import           Data.Conduit ((=$=), ($$))
+import qualified Data.Conduit.List as C
+import           Data.List (unfoldr)
+import           Data.Monoid
+import           Data.Store (PeekException (..))
+import           Data.Store.Streaming
 import qualified System.IO.ByteBuffer as BB
+import           Test.Hspec
+import           Test.Hspec.SmallCheck
+import           Test.SmallCheck
 
 spec :: Spec
 spec = do
   describe "conduitEncode and conduitDecode" $ do
     it "Roundtrips ([Int])." $ property roundtrip
     it "Roundtrips ([Int]), with chunked transfer." $ property roundtripChunked
+    it "Throws an Exception on incomplete messages." $ conduitIncomplete
   describe "peekMessage" $ do
-    it "demands more input when needed." $ property askMore
+    it "demands more input when needed." $ property (askMore 8)
+    it "demands more input on incomplete SizeTag." $ property (askMore 1)
     it "successfully decodes valid input." $ property peek
+  describe "decodeMessage" $
+    it "Throws an Exception on incomplete messages." $ decodeIncomplete
 
 roundtrip :: [Int] -> Property IO
 roundtrip xs = monadic $ do
@@ -48,14 +54,28 @@ roundtripChunked xs = monadic $ do
     $$ C.consume
   return $ xs' == xs
 
-askMore :: Integer -> Property IO
-askMore x = monadic $ BB.withByteBuffer 10 $ \ bb -> do
+conduitIncomplete :: Expectation
+conduitIncomplete =
+    (runResourceT (C.sourceList [incompleteInput]
+                  =$= conduitDecode 10
+                  $$ C.consume)
+    :: IO [Message Integer]) `shouldThrow` peekException
+
+-- splits an encoded message after n bytes.  Feeds the first part to
+-- peekResult, expecting it to require more input.  Then, feeds the
+-- second part and checks if the decoded result is the original
+-- message.
+askMore :: Int -> Integer -> Property IO
+askMore n x = monadic $ BB.withByteBuffer 10 $ \ bb -> do
   let bs = encodeMessage (Message x)
-      bs' = snd . BS.splitAt (BS.length bs -2) $ bs
-  BB.copyByteString bb bs'
+      (start, end) = BS.splitAt n $ bs
+  BB.copyByteString bb start
   peekResult <- peekMessage bb :: IO (PeekMessage IO Integer)
   case peekResult of
-    NeedMoreInput _ -> return True
+    NeedMoreInput cont ->
+      cont end >>= \case
+        Done (Message x') -> return $ x' == x
+        _ -> return False
     _ -> return False
 
 peek :: Integer -> Property IO
@@ -66,3 +86,17 @@ peek x = monadic $ BB.withByteBuffer 10 $ \ bb -> do
   case peekResult of
     NeedMoreInput _ -> return False
     Done (Message x') -> return $ x' == x
+
+decodeIncomplete :: IO ()
+decodeIncomplete = BB.withByteBuffer 0 $ \ bb -> do
+  BB.copyByteString bb (BS.take 1 incompleteInput)
+  (decodeMessage bb (return Nothing) :: IO (Maybe (Message Integer)))
+    `shouldThrow` peekException
+
+incompleteInput :: BS.ByteString
+incompleteInput =
+  let bs = encodeMessage (Message (42 :: Integer))
+  in BS.take (BS.length bs - 1) bs
+
+peekException :: Selector PeekException
+peekException = const True
