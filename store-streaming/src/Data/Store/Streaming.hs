d57e4ec97d9ba87b7296883650537254e4368f92
Michael Sloan <mgsloan@gmail.com>
2019-11-17 22:18:30 -0700

    Revert "Fix a few warnings"
    This reverts commit ace41e672db19aedf0b523d7d7b1c5db979746a8.

--- store-streaming/src/Data/Store/Streaming.hs
+++ store-streaming/src/Data/Store/Streaming.hs
@@ -201,7 +201,7 @@ decodeMessageFd bb fd = do
 #endif
 
 -- | Conduit for encoding 'Message's to 'ByteString's.
-conduitEncode :: (Monad m, Store a) => C.ConduitT (Message a) ByteString m ()
+conduitEncode :: (Monad m, Store a) => C.Conduit (Message a) m ByteString
 conduitEncode = C.map encodeMessage
 
 -- | Conduit for decoding 'Message's from 'ByteString's.
@@ -210,7 +210,7 @@ conduitDecode :: (MonadResource m, Store a)
               -- ^ Initial length of the 'ByteBuffer' used for
               -- buffering the incoming 'ByteString's.  If 'Nothing',
               -- use the default value of 4MB.
-              -> C.ConduitT ByteString (Message a) m ()
+              -> C.Conduit ByteString m (Message a)
 conduitDecode bufSize =
     C.bracketP
       (BB.new bufSize)
